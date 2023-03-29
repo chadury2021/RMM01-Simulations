42731e03ba3d3e588375563eaa6eebe42c280b0c
sbneo2022 <97976373+sbneo2022@users.noreply.github.com>
2023-03-29 18:28:32 +0100

    fix imports

--- rmms-py/modules/cfmm.py
+++ rmms-py/modules/cfmm.py
@@ -8,7 +8,7 @@ import scipy
 from scipy.stats import norm
 from scipy import optimize
 import numpy as np
-from utils import nonnegative, quantilePrime, blackScholesCoveredCallSpotPrice
+from modules.utils import nonnegative, quantilePrime, blackScholesCoveredCallSpotPrice
 
 EPSILON = 1e-8
 
@@ -42,15 +42,16 @@ class CoveredCallAMM(object):
         """
         self.reserves_risky = initial_x
         self.K = K
-        self.sigma = sigma 
+        self.sigma = sigma
         self.tau = tau
         self.initial_tau = tau
         self.invariant = 0
-        self.reserves_riskless = self.K*norm.cdf(norm.ppf(1-initial_x) - self.sigma*np.sqrt(self.tau))
+        self.reserves_riskless = self.K * \
+            norm.cdf(norm.ppf(1-initial_x) - self.sigma*np.sqrt(self.tau))
         self.fee = fee
         self.accured_fees = [0, 0]
 
-    def getRisklessGivenRisky(self, risky): 
+    def getRisklessGivenRisky(self, risky):
         return self.invariant + self.K*norm.cdf(norm.ppf(1 - risky) - self.sigma*np.sqrt(self.tau))
 
     def getRisklessGivenRiskyNoInvariant(self, risky):
@@ -70,13 +71,15 @@ class CoveredCallAMM(object):
         """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        new_reserves_riskless = self.getRisklessGivenRisky(self.reserves_risky + gamma*amount_in)
+        new_reserves_riskless = self.getRisklessGivenRisky(
+            self.reserves_risky + gamma*amount_in)
         amount_out = self.reserves_riskless - new_reserves_riskless
         self.reserves_risky += amount_in
         self.reserves_riskless -= amount_out
         assert nonnegative(new_reserves_riskless)
         # Update invariant
-        self.invariant = self.reserves_riskless - self.getRisklessGivenRiskyNoInvariant(self.reserves_risky) 
+        self.invariant = self.reserves_riskless - \
+            self.getRisklessGivenRiskyNoInvariant(self.reserves_risky)
         effective_price_in_riskless = amount_out/amount_in
         return amount_out, effective_price_in_riskless
 
@@ -91,14 +94,15 @@ class CoveredCallAMM(object):
         """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        new_reserves_riskless = self.getRisklessGivenRisky(self.reserves_risky + gamma*amount_in)
+        new_reserves_riskless = self.getRisklessGivenRisky(
+            self.reserves_risky + gamma*amount_in)
         if new_reserves_riskless <= 0 or math.isnan(new_reserves_riskless):
             return 0, 0
         assert nonnegative(new_reserves_riskless)
         amount_out = self.reserves_riskless - new_reserves_riskless
         if amount_in == 0:
             effective_price_in_riskless = inf
-        else: 
+        else:
             effective_price_in_riskless = amount_out/amount_in
         return amount_out, effective_price_in_riskless
 
@@ -114,17 +118,19 @@ class CoveredCallAMM(object):
         """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        new_reserves_risky = self.getRiskyGivenRiskless(self.reserves_riskless + gamma*amount_in)
+        new_reserves_risky = self.getRiskyGivenRiskless(
+            self.reserves_riskless + gamma*amount_in)
         assert nonnegative(new_reserves_risky)
         amount_out = self.reserves_risky - new_reserves_risky
         assert nonnegative(amount_out)
         self.reserves_riskless += amount_in
         self.reserves_risky -= amount_out
         # Update invariant
-        self.invariant = self.reserves_riskless - self.getRisklessGivenRiskyNoInvariant(self.reserves_risky)
+        self.invariant = self.reserves_riskless - \
+            self.getRisklessGivenRiskyNoInvariant(self.reserves_risky)
         if amount_in == 0:
             effective_price_in_riskless = inf
-        else: 
+        else:
             effective_price_in_riskless = amount_in/amount_out
         return amount_out, effective_price_in_riskless
 
@@ -140,14 +146,15 @@ class CoveredCallAMM(object):
         """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        new_reserves_risky = self.getRiskyGivenRiskless(self.reserves_riskless + gamma*amount_in)
+        new_reserves_risky = self.getRiskyGivenRiskless(
+            self.reserves_riskless + gamma*amount_in)
         if new_reserves_risky <= 0 or math.isnan(new_reserves_risky):
             return 0, 0
         assert nonnegative(new_reserves_risky)
         amount_out = self.reserves_risky - new_reserves_risky
         if amount_out == 0:
             effective_price_in_riskless = inf
-        else: 
+        else:
             effective_price_in_riskless = amount_in/amount_out
         return amount_out, effective_price_in_riskless
 
@@ -167,7 +174,7 @@ class CoveredCallAMM(object):
         """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        R = self.reserves_risky 
+        R = self.reserves_risky
         K = self.K
         sigma = self.sigma
         tau = self.tau
@@ -182,16 +189,16 @@ class CoveredCallAMM(object):
         """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        R = self.reserves_riskless 
+        R = self.reserves_riskless
         k = self.invariant
-        K = self.K  
+        K = self.K
         sigma = self.sigma
         tau = self.tau
         if ((gamma * norm.pdf(norm.ppf(float((R + gamma*amount_in - k)/K)) + sigma*np.sqrt(tau))*quantilePrime(
                 (R + gamma*amount_in - k)/K)*(1/K)) < EPSILON):
             # Infinity
             return 1e8
-        else: 
+        else:
             return 1/(gamma * norm.pdf(norm.ppf(float((R + gamma*amount_in - k)/K)) + sigma*np.sqrt(tau))*quantilePrime(
                 (R + gamma*amount_in - k)/K)*(1/K))
 
